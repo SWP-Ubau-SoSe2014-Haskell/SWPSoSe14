f4ed5cf8531dee8af89a1dad988923fd9761ea8e
Nicolas Lehmann <mail@nicolaslehmann.de>
2014-05-01 16:00:46 +0200

    InterfaceDT adjusted for the groups Preprocessor and Lexer.

--- program/InterfaceDT.hs
+++ program/InterfaceDT.hs
@@ -1,13 +1,18 @@
 ﻿module InterfaceDT
  where
   
+  -- type definitions --
+  type Grid2D  = [String]
+  type LexNode = (Int, String, Int, Int)
+  type Graph   = [LexNode]
+  
   -- interface datatypes --
-  data Input2PreProc     = IIP String deriving Show  -- todo!
-  data PreProc2Lexer     = IPL String deriving Show  -- todo!
-  data Lexer2SynAna      = ILS String deriving Show  -- todo!
-  data SynAna2SemAna     = ISS String deriving Show  -- todo!
-  data SemAna2InterCode  = ISI String deriving Show  -- todo!
-  data InterCode2CodeOpt = IIC String deriving Show  -- todo!
-  data CodeOpt2Backend   = ICB String deriving Show  -- todo!
-  data Backend2Output    = IBO String deriving Show  -- todo!
+  data Input2PreProc     = IIP String   deriving Show
+  data PreProc2Lexer     = IPL [Grid2D] deriving Show
+  data Lexer2SynAna      = ILS [Graph]  deriving Show
+  data SynAna2SemAna     = ISS String   deriving Show  -- todo!
+  data SemAna2InterCode  = ISI String   deriving Show  -- todo!
+  data InterCode2CodeOpt = IIC String   deriving Show  -- todo!
+  data CodeOpt2Backend   = ICB String   deriving Show  -- todo!
+  data Backend2Output    = IBO String   deriving Show  -- todo!
    
