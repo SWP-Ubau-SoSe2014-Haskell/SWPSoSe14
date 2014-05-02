00dc35ac9adde3e3a798bd1fedc55611e466f24f
Kristin Knorr <tini@unclouded.de>
2014-05-02 14:42:26 +0200

    Added data types

--- program/InterfaceDT.hs
+++ program/InterfaceDT.hs
@@ -3,14 +3,18 @@
   
   -- type definitions --
   type Grid2D  = [String]
-  type LexNode = (Int, String, Int, Int)
-  type Graph   = [LexNode]
+  type LexNode = (Int, Lexem, Int)
+  type Graph   = (String, [LexNode])
+  type AST     = (String, [(Int, [Lexem], Int)])
+  
+  data Lexem = Boom | EOF | Input | Output | Underflow | RType | Constant String | Push String | Pop String | Call String | Add | Divide | Multiply | Remainder | Substract | Cut | Append | Size | Nil | Cons | Breakup | Greater | Equal | Start | Finish | Junction Int deriving (Eq, Show)
+  
   
   -- interface datatypes --
   data Input2PreProc     = IIP String   deriving (Eq, Show)
   data PreProc2Lexer     = IPL [Grid2D] deriving (Eq, Show)
   data Lexer2SynAna      = ILS [Graph]  deriving (Eq, Show)
-  data SynAna2SemAna     = ISS String   deriving (Eq, Show)  -- todo!
+  data SynAna2SemAna     = ISS [AST]    deriving (Eq, Show)
   data SemAna2InterCode  = ISI String   deriving (Eq, Show)  -- todo!
   data InterCode2CodeOpt = IIC String   deriving (Eq, Show)  -- todo!
   data CodeOpt2Backend   = ICB String   deriving (Eq, Show)  -- todo!
