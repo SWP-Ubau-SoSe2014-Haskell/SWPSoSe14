2cfed63ce9105b606fcfb4953fa6994f30fa0fc1
chris91 <christopher.pockrandt@gmx.de>
2014-05-02 21:52:14 +0200

    preprocessor bugs fixed, test cases written, preprocessor should be stable now

--- program/Preprocessor.hs
+++ program/Preprocessor.hs
@@ -1,4 +1,17 @@
-﻿module Preprocessor (
+﻿{- |
+Module      :  Preprocessor.hs
+Description :  .
+Copyright   :  (c) Christopher Pockrandt, Nicolas Lehmann
+License     :  MIT
+
+Stability   :  stable
+
+Preprocessor gets the content of the input file and puts each rail function
+into a list of strings such that the first character of the first line is a
+dollar sign. Leading lines without a dollar sign in the input file are removed.
+-}
+
+module Preprocessor (
                      process   -- main function of the module "Preprocessor"
                     )
  where
@@ -11,19 +24,25 @@
  process (IDT.IIP input) = (IDT.IPL output)
   where
    output = (groupFunctionsToGrid2Ds . removeLines. lines) input
-   -- remove strings from list until first string begins with a dollar-sign
-   removeLines :: Grid2D -> Grid2D
-   removeLines [] = []
-   removeLines (line:lines)
-               | (head line == '$') = (line:lines)
-               | otherwise = removeLines lines
-			   
-   -- put every function/program into its on grid such that the dollar sign is the first character in the first line
-   groupFunctionsToGrid2Ds :: Grid2D -> [Grid2D]
-   groupFunctionsToGrid2Ds grid = helperGFtG2D grid [] []
-    where
-     helperGFtG2D [] akk1 akk2 = akk2
-     helperGFtG2D (zeile:rest) akk1 akk2
-                  | head zeile == '$' = helperGFtG2D rest [zeile] (akk2 ++ [akk1])
-                  | otherwise = helperGFtG2D rest (akk1++[zeile]) akk2
-			
+
+ -- |Removes all leading strings from list until first string begins with a
+ -- dollar sign.
+ removeLines :: Grid2D -> Grid2D
+ removeLines [] = []
+ removeLines (line:lines)
+  | null line = removeLines lines
+  | head line == '$' = line:lines
+  | otherwise = removeLines lines
+		   
+ -- |Puts every rail function/program into its on grid such that the dollar
+ -- sign is the first character in the first line.
+ groupFunctionsToGrid2Ds :: Grid2D -> [Grid2D]
+ groupFunctionsToGrid2Ds grid = helperGFtG2D grid [] []
+  where
+   helperGFtG2D :: Grid2D -> Grid2D -> [Grid2D] -> [Grid2D]
+   helperGFtG2D [] [] akk2 = akk2
+   helperGFtG2D [] akk1 akk2 = akk2 ++ [akk1]
+   helperGFtG2D ([]:rest) akk1 akk2 = helperGFtG2D rest (akk1++[[]]) akk2
+   helperGFtG2D (('$':zeilenrest):rest) [] akk2 = helperGFtG2D rest ['$':zeilenrest] akk2
+   helperGFtG2D (('$':zeilenrest):rest) akk1 akk2 = helperGFtG2D rest ['$':zeilenrest] (akk2 ++ [akk1])
+   helperGFtG2D (zeile:rest) akk1 akk2 = helperGFtG2D rest (akk1++[zeile]) akk2
--- program/TestPreProc.hs
+++ program/TestPreProc.hs
@@ -9,12 +9,11 @@
  import qualified Preprocessor        as PreProc
  
  -- functions --
- testPreProc01   = "PreProcessor: " ~: ([["$"]]) @=? (PreProc.process ["fg", "d", "$"])
- -- testPreProc02 = "PreProcessor: " ~: (erwarteter wert) @=? (PreProc.process eingabe)
- -- testPreProc03 = "PreProcessor: " ~: (erwarteter wert) @=? (PreProc.process eingabe)
- -- testPreProc04 = "PreProcessor: " ~: (erwarteter wert) @=? (PreProc.process eingabe)
- -- testPreProc05 = "PreProcessor: " ~: (erwarteter wert) @=? (PreProc.process eingabe)
- -- ...
+ testPreProc01   = "PreProc: " ~: ([]) @=? (process "")
+ testPreProc02   = "PreProc: " ~: ([]) @=? (process "a\nb\n")
+ testPreProc03   = "PreProc: " ~: ([["$1"], ["$2"]]) @=? (process "$1\n$2\n")
+ testPreProc04   = "PreProc: " ~: ([["$1"], ["$2", "", "", ""], ["$3", "", "", "", ""]]) @=? (process "$1\n$2\n\n\n\n$3\n\n\n\n\n")
+ testPreProc05   = "PreProc: " ~: ([["$2"]]) @=? (process " $1\n$2\n")
   
- testModule = [testPreProc01]--[ ,testPreProc02,testPreProc03,testPreProc04,testPreProc05]
+ testModule = [testPreProc01,testPreProc02,testPreProc03,testPreProc04,testPreProc05]
                         
