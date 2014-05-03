cfa4fb8826b4cc32fe773210653b16e0225df542
chris91 <christopher.pockrandt@gmx.de>
2014-05-03 12:24:02 +0200

    Interface merged with synsem-analysis and Interface for SemAna2InterCode added

--- program/InterfaceDT.hs
+++ program/InterfaceDT.hs
@@ -3,16 +3,19 @@
   
   -- type definitions --
   type Grid2D  = [String]
-  type LexNode = (Int, String, Int, Int)
-  type Graph   = [LexNode]
+  type LexNode = (Int, Lexeme, Int)
+  type Graph   = (String, [LexNode])
+  type AST     = (String, [(Int, [Lexeme], Int)])
+  type SymbTable = [String] -- list of variable names
+  
+  data Lexeme = Boom | EOF | Input | Output | Underflow | RType | Constant String | Push String | Pop String | Call String | Add | Divide | Multiply | Remainder | Substract | Cut | Append | Size | Nil | Cons | Breakup | Greater | Equal | Start | Finish | Junction Int deriving (Eq, Show)
   
   -- interface datatypes --
   data Input2PreProc     = IIP String   deriving (Eq, Show)
   data PreProc2Lexer     = IPL [Grid2D] deriving (Eq, Show)
   data Lexer2SynAna      = ILS [Graph]  deriving (Eq, Show)
-  data SynAna2SemAna     = ISS String   deriving (Eq, Show)  -- todo!
-  data SemAna2InterCode  = ISI String   deriving (Eq, Show)  -- todo!
+  data SynAna2SemAna     = ISS [AST]    deriving (Eq, Show)
+  data SemAna2InterCode  = ISI ([AST], SymbTable)   deriving (Eq, Show)  -- todo!
   data InterCode2CodeOpt = IIC String   deriving (Eq, Show)  -- todo!
   data CodeOpt2Backend   = ICB String   deriving (Eq, Show)  -- todo!
   data Backend2Output    = IBO String   deriving (Eq, Show)  -- todo!
-   
