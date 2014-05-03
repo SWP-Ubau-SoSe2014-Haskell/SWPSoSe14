fea0a993b7063889ee053ba13282cc8d45fbd452
chris91 <christopher.pockrandt@gmx.de>
2014-05-03 12:46:49 +0200

    comments added to interfaces regarding AST and lexemes

--- program/InterfaceDT.hs
+++ program/InterfaceDT.hs
@@ -10,6 +10,17 @@
   
   data Lexeme = Boom | EOF | Input | Output | Underflow | RType | Constant String | Push String | Pop String | Call String | Add | Divide | Multiply | Remainder | Substract | Cut | Append | Size | Nil | Cons | Breakup | Greater | Equal | Start | Finish | Junction Int deriving (Eq, Show)
   
+  -- |(NodeID (start: 1), Lexeme of Node, NodeID of following Node)
+  type LexNode = (Int, Lexeme, Int)
+  -- |(FunctionID, Graph of Function as adjacency list)
+  type Graph   = (String, [LexNode])
+  -- |(FunctionID, [(PathID (start: 1), List of Lexemes to be executed sequentially, PathID of following Path)])
+  type AST     = (String, [(Int, [Lexeme], Int)])
+  -- |* following Nodes or Pathes could have ID==0, in this case there is no Follower
+  
+  -- |Junction Int <=> if false goto Int; if true <=> following node
+  data Lexeme = Boom | EOF | Input | Output | Underflow | RType | Constant String | Push String | Pop String | Call String | Add | Divide | Multiply | Remainder | Substract | Cut | Append | Size | Nil | Cons | Breakup | Greater | Equal | Start | Finish | Junction Int deriving (Eq, Show)
+  
   -- interface datatypes --
   data Input2PreProc     = IIP String   deriving (Eq, Show)
   data PreProc2Lexer     = IPL [Grid2D] deriving (Eq, Show)
