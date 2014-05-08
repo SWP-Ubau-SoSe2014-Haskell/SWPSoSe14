ace1d0d222e4ceaf7b15fe205a17d8eb6a54f429
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-05-08 23:36:29 +0200

    fix style in Main.hs

--- program/Main.hs
+++ program/Main.hs
@@ -1,27 +1,27 @@
-﻿module Main (
+module Main (
              main  -- mainfunction to run the program
             )
- where
+where
 
- -- imports --
- import InterfaceDT                   as IDT
- import qualified Testing             as Test
- import qualified Preprocessor        as PreProc
- import qualified Lexer
- import qualified SyntacticalAnalysis as SynAna
- import qualified SemanticalAnalysis  as SemAna
- import qualified IntermediateCode    as InterCode
- import qualified CodeOptimization    as CodeOpt
- import qualified Backend
- 
- 
- -- functions --
- main :: IO()
- main = do putStrLn "Enter inputfile (path): "
-           inputfile <- getLine
-           putStrLn "Enter outputfile (path): "
-           outputfile <- getLine
-           putStrLn ("Compiling " ++ inputfile ++ " to " ++ outputfile)
-           do input <- readFile inputfile
-              let output (IBO x) = x
-              do writeFile outputfile (output ((Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.process . PreProc.process) (IIP input)))
+-- imports --
+import InterfaceDT                   as IDT
+import qualified Testing             as Test
+import qualified Preprocessor        as PreProc
+import qualified Lexer
+import qualified SyntacticalAnalysis as SynAna
+import qualified SemanticalAnalysis  as SemAna
+import qualified IntermediateCode    as InterCode
+import qualified CodeOptimization    as CodeOpt
+import qualified Backend
+
+
+-- functions --
+main :: IO()
+main = do putStrLn "Enter inputfile (path): "
+          inputfile <- getLine
+          putStrLn "Enter outputfile (path): "
+          outputfile <- getLine
+          putStrLn ("Compiling " ++ inputfile ++ " to " ++ outputfile)
+          do input <- readFile inputfile
+             let output (IBO x) = x
+             do writeFile outputfile (output ((Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.process . PreProc.process) (IIP input)))
