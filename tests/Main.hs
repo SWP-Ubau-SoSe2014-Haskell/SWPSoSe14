249cf548a179032840b629136c90d21b3c9f3b62
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-05-08 19:13:29 +0200

    add testsuite to cabal file
    Use "cabal test" to run tests. This should run all available test.

---
+++ tests/Main.hs
@@ -0,0 +1,17 @@
+﻿module Main(main) where
+
+-- imports --
+import Test.HUnit
+import InterfaceDT                   as IDT
+import qualified TestPreProc
+import qualified TestLexer
+import qualified TestSynAna
+import qualified TestSemAna
+import qualified TestInterCode
+import qualified TestCodeOpt
+import qualified TestBackend
+
+main :: IO ()
+main = do
+  counts <- runTestTT $ TestList (TestPreProc.testModule++TestLexer.testModule++TestSynAna.testModule++TestSemAna.testModule++TestInterCode.testModule++TestCodeOpt.testModule++TestBackend.testModule)
+  return ()
