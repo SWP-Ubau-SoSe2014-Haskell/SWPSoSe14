d6935ec40e84f859705d6852fdff81b1bd9466b5
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-05-09 15:47:50 +0200

    generate function dummy from AST

--- program/IntermediateCode.hs
+++ program/IntermediateCode.hs
@@ -5,10 +5,26 @@ import InterfaceDT as IDT
 
 import LLVM.General.AST
 import qualified LLVM.General.AST as AST
+import LLVM.General.AST.Global
 
-initModule :: AST.Module
-initModule = defaultModule { moduleName = "rail-heaven" }
+-- generate module from list of definitions
+generateModule :: [Definition] -> AST.Module
+generateModule definitions = defaultModule {
+  moduleName = "rail-heaven",
+  moduleDefinitions = definitions
+}
 
--- functions --
+-- generate function definition from AST
+generateFunction :: AST -> Definition
+generateFunction (name, lexemes) = GlobalDefinition $ functionDefaults {
+  name = Name name,
+  returnType = VoidType
+}
+
+-- generate list of LLVM Definitions from list of ASTs
+generateFunctions :: [AST] -> [Definition]
+generateFunctions = map generateFunction
+
+-- entry point into module --
 process :: IDT.SemAna2InterCode -> IDT.InterCode2CodeOpt
-process input = IDT.IIC initModule
+process (IDT.ISI input) = IDT.IIC $ generateModule $ generateFunctions input
