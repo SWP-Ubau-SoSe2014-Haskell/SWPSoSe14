4bfd70be06c2e1d618f2d424d18084aa558e95f5
Christoph Graebnitz <christoph.graebnitz@gmail.com>
2014-05-15 13:15:44 +0200

    Refactored textE to documentView

--- src/RailEditor/Main.hs
+++ src/RailEditor/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Graphics.UI.WX
 import Graphics.UI.WXCore
 import Data.Maybe
+import Data.Bits
 
 main::IO()
 main = start gui
@@ -12,20 +13,21 @@ gui::IO()
 gui = do
         mainFrame <- frame [text := "RailEditor"]
         set mainFrame []
-        textE <- textCtrl mainFrame []
-        set mainFrame [layout := fill (widget textE)]
+        documentView <- textCtrlRich mainFrame []
+        set mainFrame [layout := fill (widget  documentView)]
         set mainFrame [outerSize := (sz 640 480)]
-        
+        set documentView [wrap := WrapNone]
+								
         --Setup the menu 
         mnu <- menuPane [text := "file"]
         filePath <- varCreate Nothing
         menuItem mnu [text := "&Open\tCtrl+O",
                       help := "Opens an existing document",
-                      on command := openFile mainFrame textE
+                      on command := openFile mainFrame  documentView
                       ]
         menuItem mnu [text := "&Save\tCtrl+X",
                       help := "Saves a file in the choosen directory",
-                      on command := saveFile mainFrame textE
+                      on command := saveFile mainFrame  documentView
                       ]
         menuItem mnu [text := "&Quit\tCtrl+Q",
                       help := "Closes the application",
@@ -48,4 +50,4 @@ openFile mainFrame textField = do
                                  path <- fileOpenDialog mainFrame False False "openFile" [("Any file",["*.*"])] "" ""
                                  textCtrlLoadFile textField (fromJust path)
                                  return()
-                                        
+                  
