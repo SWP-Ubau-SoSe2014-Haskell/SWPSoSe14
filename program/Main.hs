2e30e68cd13b4c970edda0a235cd4d09a6b8b04e
Christopher Pockrandt <christopher.pockrandt@fu-berlin.de>
2014-05-15 11:46:47 +0200

    Main.hs restored

--- program/Main.hs
+++ program/Main.hs
@@ -25,11 +25,11 @@ where
  import InterfaceDT                   as IDT
  import qualified Preprocessor        as PreProc
  import qualified Lexer
--- import qualified SyntacticalAnalysis as SynAna
--- import qualified SemanticalAnalysis  as SemAna
--- import qualified IntermediateCode    as InterCode
--- import qualified CodeOptimization    as CodeOpt
--- import qualified Backend
+ import qualified SyntacticalAnalysis as SynAna
+ import qualified SemanticalAnalysis  as SemAna
+ import qualified IntermediateCode    as InterCode
+ import qualified CodeOptimization    as CodeOpt
+ import qualified Backend
  
   
  -- functions --
