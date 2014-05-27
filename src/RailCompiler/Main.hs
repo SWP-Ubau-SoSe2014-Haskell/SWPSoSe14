b4ec01a6cc23fc9cae2cd492bdf3210962eba68c
Tobias Kranz <tkranz@zedat.fu-berlin.de>
2014-05-27 15:12:01 +0200

    Fixed the hlint stuff (except for one redundant do, see --exportAST TODO below).
    -i, -o, -h seem to work as intended.
    
    Tested:
    compile to stdout:
    -i HelloWorld.rail -c
    compile to hello.llvm
    -i HelloWorld.rail -o hello.llvm -c
    
    Not Working yet:
    --exportAST is not working and therefore ignored at the moment (TODO: uncomment Main.hs line 86 and fix it)
    
    Untested (due to not working --exportAST):
    --importAST

--- src/RailCompiler/Main.hs
+++ src/RailCompiler/Main.hs
@@ -41,23 +41,23 @@ defaultOptions   = Options {
 options :: [OptDescr (Options -> IO Options)]
 options = [
     --Option ['V'] ["version"] (NoArg  showVersion)      "show version number",
-    Option ['h'] ["help"     ] (NoArg  showHelp         ) "display Help Text",
-    Option ['i'] ["input"    ] (ReqArg setInput  "FILE" ) "input file (don't set to use stdin')",
-    Option ['o'] ["output"   ] (ReqArg setOutput "FILE" ) "output file (don't set to use stdout')",
-    Option ['c'] ["compile"  ] (NoArg  setCompile       ) "compile 'rail' to 'llvm'",
-    Option [   ] ["exportAST"] (OptArg setExpAST "FILE" ) "export frontend AST (don't offer a File to use stdout) \n(dont set with --importAST)",
-    Option [   ] ["importAST"] (NoArg setImpAST         ) "import frontend AST and compile to llvm\nautosets -c \n(set input via -i; (dont set with --exportAST)\n\nset -c with --exportAST and get both: the AST and the llvm code"    
+    Option "h" ["help"     ] (NoArg  showHelp         ) "display Help Text",
+    Option "i" ["input"    ] (ReqArg setInput  "FILE" ) "input file (don't set to use stdin')",
+    Option "o" ["output"   ] (ReqArg setOutput "FILE" ) "output file (don't set to use stdout')",
+    Option "c" ["compile"  ] (NoArg  setCompile       ) "compile 'rail' to 'llvm'",
+    Option [] ["exportAST"] (OptArg setExpAST "FILE" ) "export frontend AST (don't offer a File to use stdout)\nautosets -c \n(dont set with --importAST)",
+    Option [] ["importAST"] (NoArg setImpAST         ) "import frontend AST and compile to llvm\nautosets -c \n(set input via -i; (dont set with --exportAST)"--"\n\nset -c with --exportAST and get both: the AST and the llvm code"    
   ]
 
 showHelp _ = do
   putStrLn "rail2llvm--haskell-compiler (development version)"
   putStr "https://github.com/SWP-Ubau-SoSe2014-Haskell/SWPSoSe14\n\n"
   putStr $ usageInfo "Usage: main [OPTION...]" options  
-  exitWith ExitSuccess
+  exitSuccess
 
 setInput  arg opt = return opt { optInput     = readFile arg }
 setOutput arg opt = return opt { optOutput    = writeFile arg }
-setExpAST arg opt = return opt { optASTOutput = writeFile (fromMaybe "stdout" arg), expAST = True }
+setExpAST arg opt = return opt { optASTOutput = writeFile (fromMaybe "stdout" arg), expAST = True, compile = True }
 setImpAST     opt = return opt { impAST       = True, compile = True }
 setCompile    opt = return opt { compile      = True }
 
@@ -65,29 +65,25 @@ setCompile    opt = return opt { compile      = True }
 main = do
   args <- getArgs
   let ( actions, nonOpts, msgs ) = getOpt RequireOrder options args
-  if msgs /= [] 
-  then do error $ concat msgs ++ usageInfo "Usage: main [OPTION...]" options  
+  if msgs /= [] then error $ concat msgs ++ usageInfo "Usage: main [OPTION...]" options  
   else if nonOpts /= [] 
-       then do error $ "unrecognized arguments: " ++ unwords nonOpts ++ "\nUsage: For basic information, try the `--help' option."
-       else do opts <- foldl (>>=) (return defaultOptions) actions
-               let Options { optInput = input, optOutput = output,  optASTOutput = outputAST, compile = cmpl, impAST = imp, expAST = exp} = opts
-               if imp && exp 
-               then do error "No export of the imported AST (Usage: For basic information, try the `--help' option.)'"
-                       exitWith ExitSuccess
-               else if cmpl
-                    then do if imp
-                            then do let transform (IBO x) = x
-                                    inputWithoutIO <- input
-                                    output <- transform $ Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.toAST $ inputWithoutIO
-                                    return(output)
-                                    if exp 
-                                    then do inputWithoutIO <- input
-                                            let output = Lexer.fromAST . Lexer.process . PreProc.process $ IIP inputWithoutIO
-                                            return(output)
-                                    else do inputWithoutIO <- input
-                                            let transform (IBO x) = x
-                                            output <- transform $ Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.process . PreProc.process $ IIP inputWithoutIO
-                                            return(output)
-                            else do error "Unexspected error"
-                    else error "Unexspected error"
-
+      then error $ "unrecognized arguments: " ++ unwords nonOpts ++ "\nUsage: For basic information, try the `--help' option."
+  else do 
+      opts <- foldl (>>=) (return defaultOptions) actions
+      let Options { optInput = input, optOutput = output,  optASTOutput = outputAST, compile = cmpl, impAST = imp, expAST = exp} = opts
+               --input >>= output
+      if imp && exp 
+          then do error "No export of the imported AST (Usage: For basic information, try the `--help' option.)'"
+                  exitSuccess
+      else if cmpl 
+          then do
+               let transform (IBO x) = x
+               inputWithoutIO <- input
+               if imp 
+                   then transform (Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.toAST $ inputWithoutIO) >>= output
+               else if exp
+                   then do 
+                        --transform (Lexer.fromAST . Lexer.process . PreProc.process $ IIP inputWithoutIO) >>= outputAST
+                        transform (Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.process . PreProc.process $ IIP inputWithoutIO) >>= output
+               else transform (Backend.process . CodeOpt.process . InterCode.process . SemAna.process . SynAna.process . Lexer.process . PreProc.process $ IIP inputWithoutIO) >>= output 
+      else error "Error. Set atleast -c or --importAST or --exportAST."
