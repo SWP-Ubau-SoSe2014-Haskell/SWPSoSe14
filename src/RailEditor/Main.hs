5aee6b5f55235fb94ebe16f0dd5f0da8ad005954
m273d15 <kelvin.glass@fu-berlin.de>
2014-06-04 18:05:04 +0200

    Modularization of the code
    created:
        Main - only the initialisation of the main Window
        Menu - the menu-code
        TextArea - the code related to the textarea

--- src/RailEditor/Main.hs
+++ src/RailEditor/Main.hs
@@ -5,93 +5,25 @@ import Data.Map as Map
 import Control.Monad.Trans (liftIO)
 import Data.IORef
 import Data.Maybe
+import Menu
+import TextArea
 
-data TextArea = TextArea Layout (IORef (Int,Int)) (IORef (Map.Map (Int,Int) Entry)) (IORef (Int,Int))
-
-getLayout (TextArea layout _ _ _) = layout
-
-getPointerToCurrentInFocus (TextArea _ current _ _) = current
-
-getPointerToEntryMap (TextArea _ _ map _) = map
-
-getPointerToSize (TextArea _ _ _ size) = size
 
 main :: IO()
 main = do
     initGUI
     window <- windowNew
+    windowSetDefaultSize window 840 550
+    windowSetPosition window WinPosCenter
     layout <- layoutNew Nothing Nothing
+    menuBar <- createMenu window
+    table <- tableNew 0 0 False
+    tableAttach table menuBar 0 1 0 1 [] [] 0 0
+    layoutPut layout table 0 0
     textArea <- textAreaNew layout 10 10
-    set window [ windowDefaultWidth  := 500,
-                 windowDefaultHeight := 200,
-                 containerChild      := layout ]
+    set window [containerChild := layout ]
     onDestroy window mainQuit
     widgetShowAll window
     mainGUI
     return ()
-
-textAreaNew :: Layout -> Int -> Int -> IO(TextArea)
-textAreaNew layout x y = do
-    currentInFocus <- newIORef (0,0)
-    hashMap <- newIORef Map.empty
-    size <- newIORef (0,0)
-    let area =  TextArea layout currentInFocus hashMap size
-    createTextArea area x y
-    return area
-
-createTextArea :: TextArea -> Int -> Int -> IO()
-createTextArea area@(TextArea layout current hmap size) x y = do
-    createTextAreaH area 0 (pred x) 0 (pred y)
-    writeIORef size (x,y)
-    return ()
-
-createTextAreaH :: TextArea -> Int -> Int -> Int -> Int -> IO()
-createTextAreaH area@(TextArea layout current hamp size) xnr xnrS ynr ynrS = do
-    (maxX,maxY) <- readIORef size
-    if xnr == xnrS && ynr == ynrS
-    then do 
-        entryInsert area xnrS xnrS
-    else if xnr == xnrS && ynr < ynrS
-        then do
-            entryInsert area  xnr ynr
-            createTextAreaH area 0 xnrS (succ ynr) ynrS
-        else do
-            entryInsert area xnr ynr
-            createTextAreaH area (succ xnr) xnrS ynr ynrS
-
-entryInsert :: TextArea -> Int -> Int -> IO()
-entryInsert area@(TextArea layout current hMap size) x y = do
-    entry <- entryNew
-    set entry [entryWidthChars := 1]
-    entrySetMaxLength entry 1
-    entrySetHasFrame entry False
-    entry `on` focusInEvent $ tryEvent $ liftIO $ writeIORef current (x,y)
-    layoutPut layout entry (x*10) (18*y)
-    hamp <- readIORef hMap
-    let hMapN = Map.insert (x,y) entry hamp
-    writeIORef hMap hMapN
-    on entry keyPressEvent $ do -- TODO: handle Shift_L/Shift_R + dollar,ISO_Level3_Shift + backslash .....
-        key <- eventKeyName
-        liftIO $ if isSimpleChar key
-            then do
-                set entry [entryText := key]
-                hmap <- readIORef hMap
-                let nextEntry = Map.lookup (x+1,y) hmap
-                if not $ isJust nextEntry
-                then do
-                    let nextEntry = Map.lookup (0,y+1) hmap
-                    if not $ isJust nextEntry
-                    then return False
-                    else do
-                        let nEntry = fromJust nextEntry
-                        widgetGrabFocus nEntry
-                        return True
-                else do
-                    let nEntry = fromJust nextEntry
-                    widgetGrabFocus nEntry
-                    return True
-            else do
-                return False
-    return ()
-            where isSimpleChar x = elem x $ Prelude.map (\x -> [x])  (['a'..'z']++['A'..'Z']++" $\\/|-+x*")
 
