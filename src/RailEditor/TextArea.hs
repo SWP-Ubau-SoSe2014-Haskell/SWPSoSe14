275d95d2e837c835dc050056c64074e3e0e42f54
MrM0nkey <christoph.graebnitz@gmail.com>
2014-06-11 20:06:12 +0200

    Deleted highlighting from Main.hs. Added the initial highlighting function to TextArea.hs. Its does not work at the moment.

--- src/RailEditor/TextArea.hs
+++ src/RailEditor/TextArea.hs
@@ -1,5 +1,7 @@
 module TextArea where
 
+import Lexer
+import InterfaceDT
 import Graphics.UI.Gtk
 import Data.Map as Map
 import Control.Monad.Trans (liftIO)
@@ -59,9 +61,11 @@ entryInsert area@(TextArea layout current hMap size) x y = do
     let hMapN = Map.insert (x,y) entry hamp
     writeIORef hMap hMapN
     on entry keyPressEvent $ do -- TODO: handle Shift_L/Shift_R + dollar,ISO_Level3_Shift + backslash .....
-        key <- eventKeyName
+        key <- eventKeyName 
         liftIO $ if isSimpleChar key
             then do
+                grid2D <- buildGrid2d area (0,0) []
+                highlight area grid2D start
                 set entry [entryText := key]
                 hmap <- readIORef hMap
                 let nextEntry = Map.lookup (x+1,y) hmap
@@ -78,6 +82,8 @@ entryInsert area@(TextArea layout current hMap size) x y = do
                     widgetGrabFocus nEntry
                     return True
             else do
+                grid2D <- buildGrid2d area (0,0) []
+                highlight area grid2D start 
                 return False
     return ()
             where isSimpleChar x = elem x $ Prelude.map (\x -> [x])  (['a'..'z']++['A'..'Z']++" $\\/|-+x*")
@@ -155,3 +161,108 @@ changeColorOfCurrentEntry (TextArea _ current hMap _) color = do
     hashMap <- readIORef hMap
     let currentEntry = fromJust $ Map.lookup currentCoord hashMap
     widgetModifyText currentEntry StateNormal color
+
+-- to do different colors
+-- main highlighting function
+highlight :: TextArea -> Grid2D -> IP -> IO(IP)
+highlight textArea grid2D ip = do
+  case ip == crash of
+   True -> return ip
+   _ -> do
+    (lex, _)<- return $ parse grid2D ip
+    case lex of
+      Just NOP -> do
+            changeColorOfEntryByCoord textArea (posx ip,posy ip) ((Color 65000 0 0))
+      Just Boom -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just EOF -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Input -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Output -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Underflow -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just RType -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just (Constant _ )-> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just (Push _ )-> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just (Pop _) -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just (Call _) -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Add -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Divide -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Multiply -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Remainder -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Substract -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Cut -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Append -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Size -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Nil -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Cons -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Breakup -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Greater -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Equal -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Start -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just Finish -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Just (Junction _) -> do
+        changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 65000 0 0)
+      Nothing -> changeColorOfEntryByCoord textArea (posx ip,posy ip) (Color 0 0 0)
+    nexIP <- return $ step grid2D ip
+    print ip
+    highlight textArea grid2D nexIP
+-- builds a grid2D needed by Lexer
+buildGrid2d :: TextArea 
+    -> (Int,Int)
+    -> Grid2D
+    -> IO(Grid2D)
+buildGrid2d textArea (w,h) grid2D = do
+  (x,y) <-  readIORef $ getPointerToSize textArea
+  case h > y of
+    True -> do return grid2D
+    _ -> do
+      map <- readIORef $ getPointerToEntryMap textArea
+      grid <- buildHelp map (w,h) (x,y) grid2D
+      buildGrid2d textArea (0,h+1) grid
+
+buildHelp :: Map (Int,Int) Entry
+    -> (Int,Int)
+    -> (Int,Int)
+    -> Grid2D
+    -> IO(Grid2D)
+buildHelp map (w,h) (xMax,yMax) grid2D = do
+  case w >= xMax of
+    True -> return (grid2D)
+    _ -> do
+      (xs,x) <- return $ splitAt (length grid2D) grid2D
+      elem <- return $ Map.lookup (w,h) map
+      case isNothing(elem) of
+        True -> do
+          if x == [] 
+          then buildHelp map (w+1,h) (xMax,yMax) (xs++[" "])
+          else buildHelp map (w+1,h) (xMax,yMax) (xs++[(head x)++" "])
+        _ -> do
+          entry <- return $ fromJust elem
+          content <- entryGetText entry
+          print content
+          if x == []
+          then buildHelp map (w+1,h) (xMax,yMax) (xs++[content])
+          else buildHelp map (w+1,h) (xMax,yMax) (xs++[(head x)++content])
