13e525d380dea054e62d2ba4811620c6af14a605
Christopher P <christopher.pockrandt@fu-berlin.de>
2014-06-14 00:26:16 +0200

    #32: Header added to SemanticalAnalysis.hs

--- src/RailCompiler/SemanticalAnalysis.hs
+++ src/RailCompiler/SemanticalAnalysis.hs
@@ -1,3 +1,13 @@
+{- |
+Module      : SemanticalAnalysis.hs
+Description : .
+Maintainer  : Christopher Pockrandt
+License     : MIT
+
+There is no need for a semantical analysis at this time, therefore the function
+`process` equals the identity function
+
+-}
 module SemanticalAnalysis (
                            process   -- main function of the module "SemanticalAnalysis"
                           )
@@ -7,7 +17,9 @@ module SemanticalAnalysis (
  import InterfaceDT as IDT
  import ErrorHandling as EH
  
- -- functions --
+ -- functions
+
+ -- identity function
  process :: IDT.SynAna2SemAna -> IDT.SemAna2InterCode
  process (IDT.ISS input) = IDT.ISI output
   where
