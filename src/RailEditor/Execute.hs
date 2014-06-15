129642980421e0c96225ce63def720931f2ebe0a
root <christoph.graebnitz@gmail.com>
2014-06-15 19:22:56 +0200

    Added Execute.hs which is needed to execute binarys(compiler).

---
+++ src/RailEditor/Execute.hs
@@ -0,0 +1,17 @@
+module Execute where
+
+import Graphics.UI.Gtk
+import System.Process
+
+compile :: Window
+  -> String
+  -> IO (ProcessHandle)
+compile window outPutName = do
+  path <- get window windowTitle
+  runProcess "dist/build/SWPSoSe14/SWPSoSe14" 
+    ["-c","-i",path,"-o",outPutName]
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
