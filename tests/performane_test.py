8bc6b38fd0e8afadc7aa22769899321343b53022
Marcus Hoffmann <bubuiic@aol.com>
2014-06-27 09:51:39 +0200

    added beginning of junction performance test

--- tests/performane_test.py
+++ tests/performane_test.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 import sys
+mode=1
 
 n = int(sys.argv[1])
 f = open("perftest.rail","w")
 f.write("$ 'main'\n")
 
-for i in range(n):
-    f.write(" \\-")
-    f.write(n*"1o")
-    f.write("-\\\n")
-    f.write((4+n*2)*" "+ "|\n")
-    f.write(" /-"+n*("o1")+"-/\n")
-    f.write("|\n")
+if mode==0:
+    for i in range(n):
+        f.write(" \\-")
+        f.write(n*"1o")
+        f.write("-\\\n")
+        f.write((4+n*2)*" "+ "|\n")
+        f.write(" /-"+n*("o1")+"-/\n")
+        f.write("|\n")
+    f.write("#")
+elif mode==1:
+    n = 2 * n
+    f.write(" \\\n")
+    f.write("  \\\n")
+    for i in range(n):
+        f.write((i+3)*" "+"\\\n")
 
-f.write("#")
 f.close()
